7112e2ba2146e8cd06c300f90220787ea1b50958
Luiza <luizamonroe@gec.inatel.br>
2022-06-21 21:10:04 -0300

    first commit

---
+++ src/main/java/br/inatel/sd/labmqtt/client/SensorTemperaturaSubscriber.java
@@ -0,0 +1,31 @@
+package br.inatel.sd.labmqtt.client;
+
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class SensorTemperaturaSubscriber {
+	
+	public static void main(String[] args) throws MqttException{
+		String subscriberId = UUID.randomUUID().toString();
+		IMqttClient subscriber = new MqttClient(MyConstants.URI_BROKER, subscriberId);
+		
+		MqttConnectOptions options = new MqttConnectOptions();
+		options.setAutomaticReconnect(true);
+		options.setCleanSession(true);
+		options.setConnectionTimeout(10);
+		subscriber.connect(options);
+		
+		System.out.println("esperando por mensagens");
+		subscriber.subscribe(MyConstants.TOPIC_1, (topic, msg) -> {
+			byte[] payload = msg.getPayload();
+			System.out.println("payload: " + new String(payload));
+			System.out.println("topic: " + topic);
+		});
+		
+	}
+
+}
